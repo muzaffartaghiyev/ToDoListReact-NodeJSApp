438f017bdaef436b6ee869f27d08fd4fb612b742
muzeffertagiyev <muzaffar.taghiyev@gmail.com>
2025-08-31 19:04:48 +0300

    updated

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ require("./src/configs/dbConnection")()
 app.use(express.json())
 
 const corsOptions = {
-    origin: ['http://localhost:5173',],
+    origin: ['http://localhost:5173',"https://to-do-list-react-node-js-app-sigma.vercel.app"],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
     exposedHeaders: ['Content-Length', 'X-Custom-Header'],
@@ -36,7 +36,7 @@ app.use(cors(corsOptions));
 app.all('/', (req, res) => res.send({ message: 'Welcome to TODO API' }))
 
 
-app.use("/todos",require("./src/routers/todo.js"))
+app.use("/todos",require("./src/routers/todo"))
 
 /*---------------------------------------------*/
 // ! Error Handler
